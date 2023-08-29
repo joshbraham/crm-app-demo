80a2f3357f471184e39ca044e86b0fb0f8ac92e6
joshbraham <josh27ofto@live.co.uk>
2023-08-29 17:18:05 +0100

    Complete project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-''' Flask Web App with MS SQL '''
+""" Flask Web App with MS SQL """
 
 from platform import platform
 from flask import Flask, render_template, request
@@ -7,22 +7,23 @@ import pyodbc
 
 app = Flask(__name__)
 
-if platform().startswith('mac'):
-    connection_string = f'\
+if platform().startswith("mac"):
+    connection_string = f"\
         DRIVER={DRIVER};\
         SERVER={SERVER};\
         DATABASE={DATABASE};\
         UID={USERNAME};\
-        PWD={PASSWORD};'
+        PWD={PASSWORD};"
 else:
-    connection_string = f'\
+    connection_string = f"\
         DRIVER={DRIVER};\
         SERVER={SERVER};\
         DATABASE={DATABASE};\
-        TRUSTED_CONNECTION={TRUSTED_CONNECTION}'
+        TRUSTED_CONNECTION={TRUSTED_CONNECTION}"
 
-def execute_query(query, params=None, method='GET'):
-    ''' Define a helper function to execute SQL queries '''
+
+def execute_query(query, params=None, method="GET"):
+    """Define a helper function to execute SQL queries"""
     try:
         with pyodbc.connect(connection_string, timeout=TIMEOUT) as conn:
             try:
@@ -31,11 +32,13 @@ def execute_query(query, params=None, method='GET'):
                         cursor.execute(query, params)
                     else:
                         cursor.execute(query)
-                    if method != 'GET':
+                    if method != "GET":
                         conn.commit()
-                        cursor.execute('''
+                        cursor.execute(
+                            """
                             SELECT TOP 1 CustomerID FROM Customers ORDER BY CustomerID DESC
-                        ''')
+                        """
+                        )
                         get_id = cursor.fetchall()
                         return get_id[0][0]
                     return cursor.fetchall()
@@ -45,48 +48,89 @@ def execute_query(query, params=None, method='GET'):
         return e
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    ''' Define Index route '''
-    return render_template('index.html')
+    """Define Index route"""
+    return render_template("index.html")
 
-@app.route('/customers', methods=['GET', 'POST'])
+
+@app.route("/customers", methods=["GET", "POST"])
 def customers():
-    ''' Define /customres route '''
-    if request.method == 'GET':
+    """Define /customres route"""
+    if request.method == "GET":
         # Get all rows from the table
-        rows = execute_query('SELECT * FROM customers')
+        rows = execute_query("SELECT * FROM customers")
         if not isinstance(rows, list):
-            return render_template('error.html',
-                                    type=type(rows).__name__, message=rows)
-        return render_template('customers.html',
-                                rows=rows)
+            return render_template("error.html", type=type(rows).__name__, message=rows)
+        return render_template("customers.html", rows=rows)
 
     # Create a new row in the table
-    customer_name = request.form['CustomerName']
-    contact_name = request.form['ContactName']
-    address = request.form['Address']
-    city = request.form['City']
-    postal_code = request.form['PostalCode']
-    country = request.form['Country']
+    customer_name = request.form["CustomerName"]
+    contact_name = request.form["ContactName"]
+    address = request.form["Address"]
+    city = request.form["City"]
+    postal_code = request.form["PostalCode"]
+    country = request.form["Country"]
     customer_id = execute_query(
-        f'''
+        f"""
         INSERT INTO 
             Customers 
         (CustomerName, ContactName, Address, City, PostalCode, Country) 
         VALUES
             ('{customer_name}', '{contact_name}',
-            '{address}','{city}',
-            '{postal_code}','{country}')
-        ''',
-        method='INSERT'
-        )
+            '{address}', '{city}',
+            '{postal_code}', '{country}')
+        """,
+        method="INSERT",
+    )
     if not isinstance(customer_id, int):
-        return render_template('error.html',
-                                type=type(customer_id).__name__, message=customer_id)
-    return render_template('success.html',
-                            customer_id=customer_id, data=request.form)
+        return render_template(
+            "error.html", type=type(customer_id).__name__, message=customer_id
+        )
+    return render_template(
+        "customer-success.html", customer_id=customer_id, data=request.form
+    )
+
+
+@app.route("/suppliers", methods=["GET", "POST"])
+def suppliers():
+    """Define /proucts route"""
+    if request.method == "GET":
+        # Get all rows from the table
+        rows = execute_query("SELECT * FROM suppliers")
+        if not isinstance(rows, list):
+            return render_template("error.html", type=type(rows).__name__, message=rows)
+        return render_template("suppliers.html", rows=rows)
+
+    # Create a new row in the table
+    supplier_name = request.form["SupplierName"]
+    contact_name = request.form["ContactName"]
+    address = request.form["Address"]
+    city = request.form["City"]
+    postal_code = request.form["PostalCode"]
+    country = request.form["Country"]
+    phone = request.form["Phone"]
+    supplier_id = execute_query(
+        f"""
+        INSERT INTO 
+            Suppliers 
+        (SupplierName, ContactName, Address, City, PostalCode, Country, Phone) 
+        VALUES
+            ('{supplier_name}', '{contact_name}',
+            '{address}', '{city}',
+            '{postal_code}', '{country}', '{phone}')
+        """,
+        method="INSERT",
+    )
+    if not isinstance(supplier_id, int):
+        return render_template(
+            "error.html", type=type(supplier_id).__name__, message=supplier_id
+        )
+    return render_template(
+        "supplier-success.html", supplier_id=supplier_id, data=request.form
+    )
+
 
 # Run the app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
